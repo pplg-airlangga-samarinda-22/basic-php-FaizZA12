3fd9676a2e9ad9c0844e94ca6deb95ce4db22e07
Faiz <130257934+RenMorii@users.noreply.github.com>
2025-01-07 09:26:24 +0800

    basic-php-FaizZA12

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
+    // simpan nama dan nisn kalian ke dalam sebuah variable
+    $nama = "Faiz Ziyad Abdilah";
+    $nisn = "0068996735";
 
+    echo "Nama: " . $nama . "<br>";
+    echo "NISN: " . $nisn;
 ?>
